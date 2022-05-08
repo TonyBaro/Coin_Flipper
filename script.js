3f72b344a6e572a1c536a60010b87a0bc32e12d4
TonyB <tonybaromagic@gmail.com>
2022-05-07 21:55:58 -0400

    added flip

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 console.log("page loaded")
 
-var Coin="heads"
+var Coin="Heads"
+
+function fullFlip(){
+    drawCoin()
+    setTimeout(flip,4999)
+}
 
 function flip(){
     var x=Math.floor(Math.random()*(2))
     console.log(x)
     if (x==0){
+        Coin = "Heads"
         document.querySelector(".flip").innerHTML=
-        `<img id=coin onmousedown="flip()" src="./assets/heads.png" alt="heads">
-        <p>Heads</p>`
+        `<img id=coin onmousedown="fullFlip()" src="./assets/heads.png" alt="heads">
+        <p>${Coin}</p>`
         console.log("heads")
+        
 
     }
     if (x==1){
+        Coin = "Tails"
         document.querySelector(".flip").innerHTML=
-        `<img id=coin onmousedown="flip()" src="./assets/tails.png" alt="tails">
-        <p>Tails</p>`
+        `<img id=coin onmousedown="fullFlip()" src="./assets/tails.png" alt="tails">
+        <p>${Coin}</p>`
         console.log("tails")
 
     }
+}
+
+function drawCoin(){
+    document.getElementById("coin").style.transform="rotateX(720deg)";
 }
 console.log(document.querySelector(".flip").innerHTML)
